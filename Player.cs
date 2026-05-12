35f1063ef79299b1638797584f2ee687c20348d9
naneaghabekyan <naneaghabekyan201@gmail.com>
2026-05-12 18:12:18 +0400

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerGameLobbySystem
+{
+    public class Player
+    {
+        public string Nickname { get; set; }
+        private string password;
+        public string Password
+        {
+            set
+            {
+                password = value;
+            }
+        }
+        public Player(string nick,string pass)
+        {
+            Nickname = nick;
+            Password = pass;
+        }
+    }
+}
---
+++ RegisterPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerGameLobbySystem
+{
+    public class RegisterPlayer
+    {
+
+        public List<Player> players = new List<Player>();
+        public void Register(Player player)
+        {
+            players.Add(player);
+        }
+        
+    }
+}
