21553fccbc32cfc3b9394b18dce231e4e04dee48
Yelena-Hovhannisyan <hovhannisyan.elena.06@gmail.com>
2026-05-14 18:15:43 +0400

    Add files via upload

---
+++ CasualMatch.cs
@@ -0,0 +1,35 @@
+﻿using MultiplayerGameLobbySystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MultiplayerGameLobbySystem
+{
+    public class CasualMatch : MatchGame
+    {
+        public CasualMatch(Player p1, Player p2)
+        : base(p1, p2)
+        {
+        }
+        public override void Start()
+        {
+            Console.WriteLine($"{Player1.Nickname} and {Player2.Nickname} started a casual match!\n ");
+        }
+
+        public override void GamePlay()
+        {
+            Game newGame = new Game();
+
+            newGame.GameLogic(Player1, Player2);
+        }
+
+        public override void End()
+        {
+            Console.WriteLine($"{Player1.Nickname} and {Player2.Nickname} finished the casual match!\n ");
+        }
+    }
+}
---
+++ RankedMatch.cs
@@ -0,0 +1,37 @@
+﻿using MultiplayerGameLobbySystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MultiplayerGameLobbySystem
+{
+
+    public class RankedMatch : MatchGame
+    {
+        public RankedMatch(Player p1, Player p2)
+        : base(p1, p2)
+        {
+        }
+        public override void Start()
+        {
+            Console.WriteLine($"{Player1.Nickname} and {Player2.Nickname} started a ranked match!\n ");
+        }
+
+        public override void GamePlay()
+        {
+            Game newGame = new Game();
+
+            (newGame.GameLogic(Player1, Player2)).Rank += 80;
+        }
+
+        public override void End()
+        {
+            Console.WriteLine($"{Player1.Nickname} and {Player2.Nickname} finished the ranked match!\n ");
+            Console.WriteLine($"Ranks:\n {Player1.Nickname}: {Player1.Rank}\n {Player2.Nickname}: {Player2.Rank}");
+        }
+    }
+}
